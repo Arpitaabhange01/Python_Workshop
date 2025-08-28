c8980f20d55c117c8cb30cc37e49b524d4e20ec9
Heisenberg <heisenberg.bkl03@gmail.com>
2025-08-28 11:37:27 -0700

    5.py

---
+++ BHARGAV_03/Day.02/5.py
@@ -0,0 +1,7 @@
+# Calculate the area of circle 
+
+import math
+print("Calculate the area of circle")
+radius=int(input("enter the radius of circle:"))
+area=math.pi * radius * radius
+print("area of circle is:-",area)
