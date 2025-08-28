b4940bb8e45f018a6d1b807701baad02c139e12b
MayurShrigiri <79790822+MayurShrigiri@users.noreply.github.com>
2025-08-28 03:14:50 -0700

    Update Program_12.py

--- Mayur_24/Day_02/Program_12.py
+++ Mayur_24/Day_02/Program_12.py
@@ -1,4 +1,4 @@
-# 2. Take 2 numbers and print their sum
+# 12. Take 2 numbers and print their sum
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 sum=num1+num2
