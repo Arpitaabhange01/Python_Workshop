67c288328fc61c05eea6dc897c318444afc8acfc
MayurShrigiri <79790822+MayurShrigiri@users.noreply.github.com>
2025-08-28 03:40:50 -0700

    Create Program_20.py

---
+++ Mayur_24/Day_02/Program_20.py
@@ -0,0 +1,8 @@
+# 10. Take three subjects marks and calculate total and average
+mark1 = float(input("Enter marks for subject 1: "))
+mark2 = float(input("Enter marks for subject 2: "))
+mark3 = float(input("Enter marks for subject 3: "))
+total = mark1 + mark2 + mark3
+average = total / 3
+print("Total marks:", total)
+print("Average marks:", average)
