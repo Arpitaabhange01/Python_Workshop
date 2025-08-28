0ec67979d5d059fe7d9fabef6293e3787a125de0
srushtijamadar06-cell <srushtijamadar06@gmail.com>
2025-08-28 16:24:18 +0530

    Create Ten.py

---
+++ Srushti Jamadar 11/Day 01/Ten.py
@@ -0,0 +1,4 @@
+#Print your college name in upper case and lower case
+ College= Vidya Vikas Pratishthan Institute of Engineering & Technology
+      print("college name in upper case:",College.upper())
+      print("college name in lower case:",College.lower()) 
