70f9e3bf0c779558e1d6606fb97df63c8b9965ab
Heisenberg <heisenberg.bkl03@gmail.com>
2025-08-28 11:21:59 -0700

    4.py

--- BHARGAV_03/Day.02/4.py
+++ BHARGAV_03/Day.02/4.py
@@ -1,8 +1,8 @@
 #calculate the area of rectangle.
 
-print("Area of rectangle")
-l=int(input("enter the length:"))
-b=int(input("enter the breath:"))
-area=l*b
+print("Calculate Area of rectangle")
+l=int(input("enter the length of rectangle:"))
+w=int(input("enter the widthof rectangle:"))
+area=l*w
 print("area of rectangle is",area)
 
