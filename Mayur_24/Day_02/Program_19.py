396e339af89be1360300231d448ec9de1aff674d
MayurShrigiri <79790822+MayurShrigiri@users.noreply.github.com>
2025-08-28 03:34:50 -0700

    Create Program_19.py

---
+++ Mayur_24/Day_02/Program_19.py
@@ -0,0 +1,19 @@
+# 19. Simple calculator (add, sub, multi, div)
+print("Simple Calculator")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    print("Result:", num1 + num2)
+elif operation == '-':
+    print("Result:", num1 - num2)
+elif operation == '*':
+    print("Result:", num1 * num2)
+elif operation == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero!")
+else:
+    print("Invalid operation!")
+print("Thank You")
