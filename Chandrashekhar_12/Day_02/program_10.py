1c4e65e45e36a3862fae41e37e616842ced3cb09
chandarshekharkalshetti <chandarshekharkalshetti@gmail.com>
2025-08-28 07:10:59 -0400

    Create program_10.py

---
+++ Chandrashekhar_12/Day_02/program_10.py
@@ -0,0 +1,7 @@
+#take three subject marks and calculate total and average
+sub1=float(input("Enter the marks"))
+sub2=float(input("Enter the marks"))
+sub3=float(input("Enter the marks"))
+total=sub1+sub2+sub3
+avg=total/3
+print("Total=",total ,"average=",avg )
