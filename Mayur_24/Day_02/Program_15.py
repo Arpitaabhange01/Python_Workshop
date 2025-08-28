d6c7ae169e139bd7dd275b5baddaf1009e497a54
MayurShrigiri <79790822+MayurShrigiri@users.noreply.github.com>
2025-08-28 03:15:33 -0700

    Update Program_15.py

--- Mayur_24/Day_02/Program_15.py
+++ Mayur_24/Day_02/Program_15.py
@@ -1,4 +1,4 @@
-# 5. Calculate area of rectangle
+# 15. Calculate area of rectangle
 length = float(input("Enter length of rectangle: "))
 width = float(input("Enter width of rectangle: "))
 area = length * width
